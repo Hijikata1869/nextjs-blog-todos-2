bca6ab55368be8f9786089874211b7a973ba64a2
Hijikata1869 <liv.knk@gmail.com>
2024-03-07 20:06:32 +0900

    taskの新規作成とアップデートの関数を作成

--- components/Taks.js
+++ components/Taks.js
@@ -1,9 +1,12 @@
 import Link from "next/link";
 import Cookie from "universal-cookie";
+import { useContext } from "react";
+import { StateContext } from "../context/StateContext";
 
 const cookie = new Cookie();
 
 export default function Task({ task, taskDeleted }) {
+  const { setSelectedTask } = useContext(StateContext);
   const deleteTask = async () => {
     await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/tasks/${task.id}`, {
       method: "DELETE",
@@ -29,6 +32,22 @@ export default function Task({ task, taskDeleted }) {
         </span>
       </Link>
       <div className="ml-20 float-right">
+        <svg
+          onClick={() => setSelectedTask(task)}
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 float-left cursor-pointer"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+          />
+        </svg>
+
         <svg
           onClick={deleteTask}
           xmlns="http://www.w3.org/2000/svg"
--- pages/task-page.js
+++ pages/task-page.js
@@ -4,6 +4,7 @@ import Task from "../components/Taks";
 import Link from "next/link";
 import { getAllTasksData } from "../lib/tasks";
 import useSWR from "swr";
+import StateContextProvider from "../context/StateContext";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/tasks`;
@@ -22,33 +23,35 @@ export default function TaskPage({ staticfilteredTasks }) {
   }, []);
 
   return (
-    <Layout title="Task page">
-      <ul>
-        {filteredTasks &&
-          filteredTasks.map((task) => (
-            <Task key={task.id} task={task} taskDeleted={mutate} />
-          ))}
-      </ul>
-      <Link href="/main-page">
-        <div className="flex cursor-pointer mt-5">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6 mr-3"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5"
-            />
-          </svg>
-          <span>Back to main page</span>
-        </div>
-      </Link>
-    </Layout>
+    <StateContextProvider>
+      <Layout title="Task page">
+        <ul>
+          {filteredTasks &&
+            filteredTasks.map((task) => (
+              <Task key={task.id} task={task} taskDeleted={mutate} />
+            ))}
+        </ul>
+        <Link href="/main-page">
+          <div className="flex cursor-pointer mt-5">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 mr-3"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5"
+              />
+            </svg>
+            <span>Back to main page</span>
+          </div>
+        </Link>
+      </Layout>
+    </StateContextProvider>
   );
 }
 
